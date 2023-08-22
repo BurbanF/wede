8e7b564389f1137d46f814118c3bb74ffe91dce5
BurbanF <139761213+BurbanF@users.noreply.github.com>
2023-08-21 18:14:13 -0700

    Update sendEmail.ts

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const endpoint = "https://api.emailjs.com/api/v1.0/email/send";
-const service_id = "service_ykqle9j";
-const template_id = "template_10515bh";
-const public_key = "Qz2f2ODJGyvV01kGm";
+const service_id = "service_a90m02h";
+const template_id = "template_wu45q84";
+const public_key = "UuLMTmBcNtY1e3wJ3";
 
 const sendEmail = (values: any) => {
   const payload = {
